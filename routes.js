3496e102d359749a6d18aad2612545f63f9edd1f
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-08-01 17:30:43 +0900

    before adding category functions

--- routes.js
+++ routes.js
@@ -4,12 +4,12 @@ const JOIN = "/join";
 const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
-
+// const CATEGORY = "/:category";
 // users
 const USERS = "/users";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
-const USER_DETAIL = "/:id"
+const USER_DETAIL = "/:id";
 
 //videos
 
@@ -20,44 +20,44 @@ const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
-    home : HOME,
-    join : JOIN,
-    login : LOGIN,
-    logout : LOGOUT,
-    search : SEARCH,
-    users : USERS,
-    userDetail : (id) => {
-        if(id){
-            return `/users/${id}`;
-        }else{
-            return USER_DETAIL;
-        }
-    },
-    editProfile : EDIT_PROFILE,
-    changePassword : CHANGE_PASSWORD,
-    videos : VIDEOS,
-    upload : UPLOAD,
-    videoDetail : (id) =>{
-        if (id){
-            return `/videos/${id}`;
-        } else{
-            return VIDEO_DETAIL;
-        }
-    },
-    editVideo : (id) => {
-        if (id){
-            return `/videos/${id}/edit`;
-        } else{
-            return EDIT_VIDEO;
-        }
-    },
-    deleteVideo : (id) => {
-        if (id){
-            return `/videos/${id}/delete`;
-        } else{
-            return DELETE_VIDEO;
-        }
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
     }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
 };
 
 export default routes;
