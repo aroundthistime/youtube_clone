2de77a0cb90b0acfc322e82dfb8ed76d6ef62749
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-08-02 20:52:04 +0900

    Tried hiding video descriptions on video details
    todo :  check the font-sizes in actual mobile environments, try to give animations to hiding and showing the descriptions

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,31 +2,49 @@ import { reset } from "nodemon";
 import { response } from "express";
 import routes from "../routes";
 import { render } from "pug";
+import User from "../models/User";
+import passport from "passport";
 
 export const getJoin = (req, res) => {
-    res.render("join", {pageTitle : "Join"})
+  res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
-    const {
-        body : {name, email, password, password2}
-    } = req;
-    if (password !== password2){
-        res.status(400);
-        res.render("join", {pageTitle : "Join"});
-    } else{
-        //회원가입되었을 때 정보 등록 + 로그인시키기
-        res.redirect(routes.home);
+export const postJoin = async (req, res, next) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
     }
-}
-export const getLogin = (req, res) => res.render("login", {pageTitle : "Login"});
-export const postLogin = (req, res) => {
-    res.redirect(routes.home)
-}
-export const logout = (req, res) => {
-    //do logout
-    res.redirect(routes.home);
+
+    //회원가입되었을 때 정보 등록 + 로그인시키기
+  }
 };
-export const users = (req, res) => res.render("users", {pageTitle : "Users"});
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "user Details"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "Profile edit"});
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle : "Change Password"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
+export const logout = (req, res) => {
+  req.logout();
+  res.redirect(routes.home);
+};
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "user Details" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Profile edit" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
