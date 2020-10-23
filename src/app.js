661a94235d9dc33863946e5fa23faa5444773b8c
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-10-24 05:15:16 +0900

    changeUploads

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const CookieStore = MongoStore(session);
 app.use(helmet());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static(path.join(__dirname, "static")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(cookieParser());
