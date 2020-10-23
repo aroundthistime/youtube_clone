a2b5442ec5b6ab8e2e74b3018aa4c9f8d99db67b
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2020-10-24 05:02:26 +0900

    undo

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,24 +1,27 @@
 import routes from "./routes";
 import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+// import multerS3 from "multer-s3";
+// import aws from "aws-sdk";
 
-const s3 = new aws.S3({
-  accessKey : process.env.AWS_KEY,
-  secretAccessKey : process.env.AWS_PRIVATE_KEY,
-  region : "ap-northeast-2"
-});
+// const s3 = new aws.S3({
+//   accessKey : process.env.AWS_KEY,
+//   secretAccessKey : process.env.AWS_PRIVATE_KEY,
+//   region : "ap-northeast-2"
+// });
 
-const multerVideo = multer({ storage : multerS3({
-  s3,
-  acl : "public-read",
-  bucket : "aroundthistimeyutube/video"
-}) });
-const multerAvater = multer({ storage : multerS3({
-  s3,
-  acl : "public-read",
-  bucket : "aroundthistimeyutube/avatars"
-}) });
+// const multerVideo = multer({ storage : multerS3({
+//   s3,
+//   acl : "public-read",
+//   bucket : "aroundthistimeyutube/video"
+// }) });
+// const multerAvater = multer({ storage : multerS3({
+//   s3,
+//   acl : "public-read",
+//   bucket : "aroundthistimeyutube/avatars"
+// }) });
+const multerVideo = multer({ dest: "uploads/videos/" });
+const multerAvater = multer({ dest: "uploads/avatars/" });
+
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Yutube";
